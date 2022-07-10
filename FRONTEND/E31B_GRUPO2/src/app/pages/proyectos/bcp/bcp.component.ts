30d63b53a5d01a4bb215565d9a574d124a6ee6b5
aleguia <aleguia@outlook.es>
2022-07-10 02:54:23 -0500

    actualizacion de servicios

--- FRONTEND/E31B_GRUPO2/src/app/pages/proyectos/bcp/bcp.component.ts
+++ FRONTEND/E31B_GRUPO2/src/app/pages/proyectos/bcp/bcp.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ConsultasService } from 'src/app/services/consultas.service';
 import { ProyectosService } from 'src/app/services/proyectos.service';
@@ -10,30 +10,100 @@ import { ProyectosService } from 'src/app/services/proyectos.service';
   styleUrls: ['./bcp.component.css']
 })
 export class BcpComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
   sub_menu: any;
   servicioList: any[] = [];
+  clienteList: any[] = [];
+  tipoServicioList: any[] = [];
+  proyectoList: any[] = [];
+  sedeList: any[] = [];
 
-   constructor(private readonly enrutador: Router,
-    private fb:FormBuilder,
-    private readonly proyectoServicio:ProyectosService,
-    private readonly consultaServicio:ConsultasService) { }
+  formularioRegistro = this.fb.group({
+    idTipoServicio: ['', [Validators.required]],
+    idAnalista: ['', [Validators.required]],//
+    idCliente: ['004', [Validators.required]],
+    idSede: ['', [Validators.required]],
+    idProyecto: ['', [Validators.required]],
+  })
 
-   irBCP(){
+  constructor(private readonly enrutador: Router,
+    private fb: FormBuilder,
+    private readonly proyectoServicio: ProyectosService,
+    private readonly consultaServicio: ConsultasService) { }
+
+  irBCP() {
     this.enrutador.navigate(["bcp"])
-   }
+  }
 
-  
-   cargarListado(cliente:string){
-    this.proyectoServicio.__getServiciosPorProyecto(cliente).subscribe((rest:any)=>{
-      if(rest.issuccess){
-        this.servicioList=rest.data;    
-       // console.log(this.servicioList);        
+
+  cargarListado(cliente: string) {
+    this.proyectoServicio.__getServiciosPorProyecto(cliente).subscribe((rest: any) => {
+      if (rest.issuccess) {
+        this.servicioList = rest.data;
+        // console.log(rest);        
       }
     })
-   }
+  }
 
+  cargarConsultas() {
+    this.consultaServicio.__getEntidades(1).subscribe((rest: any) => {
+      if (rest.issuccess) {
+        this.clienteList = rest.data;
+      }
+    });
+
+    this.consultaServicio.__getEntidades(0).subscribe((rest: any) => {
+      if (rest.issuccess) {
+        this.tipoServicioList = rest.data;
+      }
+    });
+
+    this.consultaServicio.__getEntidades(2).subscribe((rest: any) => {
+      if (rest.issuccess) {
+        this.sedeList = rest.data;
+      }
+    });
+
+    this.consultaServicio.__getEntidades(3).subscribe((rest: any) => {
+      if (rest.issuccess) {
+        this.proyectoList = rest.data;
+      }
+    });
+  }
+
+  //1 Crear un metodo para guaradar.
+  guaradarSolicitud() {
+    //1.1 Crear formulario con los atributos que pide el API
+    //1.2 Relacionar en el HTML los controles con el formulario creado.  
+    //1.3 Obtener el ID del usuario en sesión.
+    let token = sessionStorage.getItem('token');
+    if (token != null) {
+      let usuario = JSON.parse(token);
+      //1.4 Asignar el ID del analista
+      this.formularioRegistro.get('idCliente')?.enable();
+      this.formularioRegistro.patchValue({
+        idAnalista: usuario.idTrabajador
+      });
+      //1.5 Validar si el formulario tien datos. :   console.log(this.formularioRegistro.valid);
+      if (this.formularioRegistro.valid) {
+        //2 invocar al API para guardar datos.
+        this.proyectoServicio.__registrarServicio(this.formularioRegistro.value).subscribe((rest: any) => {
+          if(rest.issuccess){
+            window.location.href = "/BCP";
+          }
+        })
+
+
+      }
+    }
+  }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers'
+    };
     this.cargarListado("BCP");
+    this.cargarConsultas();
+    this.formularioRegistro.get('idCliente')?.disable();
   }
 }
--- FRONTEND/E31B_GRUPO2/src/app/services/proyectos.service.ts
+++ FRONTEND/E31B_GRUPO2/src/app/services/proyectos.service.ts
@@ -12,8 +12,8 @@ export class ProyectosService {
     return this.http.get<any>('https://localhost:5001/api/Servicio/ObtenerServiciosPorCliente?cliente='+data);
   }
   
-  __registrarServicio(data:any[]){
-    return this.http.post<any>('http://localhost:5000/api/Servicio/Registrar',data);
+  __registrarServicio(data:any){
+    return this.http.post<any>('https://localhost:5001/api/Servicio/Registrar',data);
   }
 }
 
